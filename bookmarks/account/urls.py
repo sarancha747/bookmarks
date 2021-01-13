666f5ba74b1aff5b0bf27c39b397a48ef3b08eac
sarancha747 <sarancha747@gmail.com>
2021-01-13 16:31:23 +0200

    registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #path('login/', views.user_login, name='login')
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
     path('', views.dashboard, name='dashboard'),
     #change password urls
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
