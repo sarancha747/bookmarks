4837377041814e73659ff70c98d440435f1d25c3
sarancha747 <sarancha747@gmail.com>
2021-02-10 15:01:27 +0200

    email aythentication

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 @login_required
@@ -51,13 +52,25 @@ def register(request):
 
 @login_required
 def edit(request):
+    profile = load_profile(request.user)
     if request.method == "POST":
         user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        profile_form = ProfileEditForm(instance=profile, data=request.POST, files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
         else:
-            user_form = UserEditForm(instance=request.user)
-            profile_form = ProfileEditForm(instance=request.user.profile)
-        return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
+
+def load_profile(user):
+    try:
+        return user.profile
+    except:
+        profile = Profile.objects.create(user=user)
+        return profile
